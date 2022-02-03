afa04676e250d21a28ca8dfa9a57b9ca7f22133e
vivek <vivekrsq@gmail.com>
2022-02-03 14:52:27 +0530

    finally form data submitted to mongo atlas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const database = require("./db/conn");
 const Student = require('./models/student');
 const app = express();
+app.use(cors());
 app.use(express.json());
-const port = 3000
+const port = 3001
 app.post('/create', (req, res)=>{
     console.log(req.body)
     const user = new Student(req.body)
