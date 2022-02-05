90bc71986e19cac134da32f5d757c7b0737ad1f6
vivek <vivekrsq@gmail.com>
2022-02-05 16:20:21 +0530

    query passed

--- index.js
+++ index.js
@@ -6,14 +6,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3001
-app.post('/create', (req, res)=>{
+app.post('/students', (req, res)=>{
     console.log(req.body)
     const user = new Student(req.body)
     user.save().then(()=>{
         res.send(user);
     }).catch(e => res.send(e));
-    
-    
+       
+});
+app.get('/students', async (req, res)=>{
+    try {
+        console.log(req.body)
+        const user = await Student.find();
+        res.send(user);
+    } catch (error) {
+        res.send(error);
+    }  
+});
+
+app.get('/students/:name', async (req, res)=>{
+    try {
+        const name = req.params.name;
+        
+        const user = await Student.find({name: name});
+        if(!user){
+            res.status(404).send();
+        }else{
+            res.send(user);
+        }
+    } catch (error) {
+        res.send(error);
+    }  
 });
 
 app.listen(port, () => {
